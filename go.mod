09843818b6c3ad5c97a7f3253c0dbbed7bdd53e2
zhanjuzhang <zhanjuzhang@wps.cn>
2023-04-03 22:20:20 +0800

    feat: id case

--- go.mod
+++ go.mod
@@ -4,6 +4,13 @@ go 1.18
 
 require (
 	github.com/bytedance/godlp v1.2.15
+	github.com/chilts/sid v0.0.0-20190607042430-660e94789ec9
+	github.com/kjk/betterguid v0.0.0-20170621091430-c442874ba63a
+	github.com/oklog/ulid v1.3.1
+	github.com/rs/xid v1.4.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/segmentio/ksuid v1.0.4
+	github.com/sony/sonyflake v1.1.0
 	github.com/speps/go-hashids/v2 v2.0.1
 )
 
--- id/id_test.go
+++ id/id_test.go
@@ -1,1 +1,75 @@
 package id
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/chilts/sid"
+	"github.com/kjk/betterguid"
+	"github.com/oklog/ulid"
+	"github.com/rs/xid"
+	uuid "github.com/satori/go.uuid"
+	"github.com/segmentio/ksuid"
+	"github.com/sony/sonyflake"
+)
+
+func TestXid(t *testing.T) {
+	guid := xid.New()
+
+	t.Log(guid.Machine(), guid.Pid(), guid.Time(), guid.Counter(), guid.String())
+}
+
+func TestAllCase(t *testing.T) {
+	genXid()
+	genKsuid()
+	genBetterGUID()
+	genUlid()
+	genSonyflake()
+	genSid()
+	genUUIDv4()
+}
+
+func genXid() {
+	id := xid.New()
+	fmt.Printf("github.com/rs/xid:           %s\n", id.String())
+}
+
+func genKsuid() {
+	id := ksuid.New()
+	fmt.Printf("github.com/segmentio/ksuid:  %s\n", id.String())
+}
+
+func genBetterGUID() {
+	id := betterguid.New()
+	fmt.Printf("github.com/kjk/betterguid:   %s\n", id)
+}
+
+func genUlid() {
+	t := time.Now().UTC()
+	entropy := rand.New(rand.NewSource(t.UnixNano()))
+	id := ulid.MustNew(ulid.Timestamp(t), entropy)
+	fmt.Printf("github.com/oklog/ulid:       %s\n", id.String())
+}
+
+func genSonyflake() {
+	flake := sonyflake.NewSonyflake(sonyflake.Settings{})
+	id, err := flake.NextID()
+	if err != nil {
+		log.Fatalf("flake.NextID() failed with %s\n", err)
+	}
+	// Note: this is base16, could shorten by encoding as base62 string
+	fmt.Printf("github.com/sony/sonyflake:   %x\n", id)
+}
+
+func genSid() {
+	id := sid.Id()
+	fmt.Printf("github.com/chilts/sid:       %s\n", id)
+}
+
+func genUUIDv4() {
+	id := uuid.NewV4()
+	fmt.Printf("github.com/satori/go.uuid:   %s\n", id)
+}
