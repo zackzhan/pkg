f2994430800478e0eab810f484ce181e1d19e284
juzhang <zhanjuzhang@gmail.com>
2023-08-10 20:16:16 +0800

    lo 库使用

--- collection/collection_test.go
+++ collection/collection_test.go
@@ -1,1 +1,40 @@
 package collection
+
+import (
+	"testing"
+
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUniqBy(t *testing.T) {
+	type User struct {
+		Name string
+		Age  int
+	}
+
+	list := []User{
+		{
+			Name: "a",
+			Age:  0,
+		},
+		{
+			Name: "b",
+			Age:  1,
+		},
+		{
+			Name: "b",
+			Age:  1,
+		},
+	}
+
+	result := lo.UniqBy(list, func(i User) string {
+		return i.Name
+	})
+
+	strings := lo.Map(result, func(item User, index int) string {
+		return item.Name
+	})
+
+	assert.Equal(t, []string{"a", "b"}, strings, "user name repeat")
+}
--- go.mod
+++ go.mod
@@ -12,11 +12,13 @@ require (
 	github.com/kjk/betterguid v0.0.0-20170621091430-c442874ba63a
 	github.com/oklog/ulid v1.3.1
 	github.com/rs/xid v1.4.0
+	github.com/samber/lo v1.38.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/segmentio/ksuid v1.0.4
 	github.com/smartystreets/goconvey v1.8.1
 	github.com/sony/sonyflake v1.1.0
 	github.com/speps/go-hashids/v2 v2.0.1
+	github.com/stretchr/testify v1.7.0
 	gopkg.in/antage/eventsource.v1 v1.0.0-20150318155416-803f4c5af225
 )
 
